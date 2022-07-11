6e63ad9d85463491adb8c9296c9665bf9eaf508f
ji-eunn <jieun.gooogle@gmail.com>
2022-07-11 23:32:55 +0900

    월의 총 일수 테스트

---
+++ src/main/java/com/project/ecommerce/controller/Test.java
@@ -0,0 +1,26 @@
+package com.project.ecommerce.controller;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+//        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        LocalDate now = LocalDate.now();
+
+//        DateTime
+
+        System.out.println(new Date());
+        System.out.println(now);
+//        cal.set(date.getYear(),date.getMonth(),date.getDate());
+
+        cal.set(now.getYear(), now.getMonth().getValue(), now.getDayOfMonth());
+        System.out.println(cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+//        System.out.println(now.getDayOfMonth());
+    }
+}
